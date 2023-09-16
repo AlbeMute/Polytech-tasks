b175700589f12d9fb0e50d6c7ff5d02113feea18
AlbeMute <lopin7653@gmail.com>
2023-09-16 20:39:21 +0300

    п

--- lesson.py
+++ lesson.py
@@ -1,3 +1,5 @@
+import math
+
 #task 1
 a = input("Ваши фамилия, имя? ")
 b = input("Сколько Вам лет?" )
@@ -5,3 +7,9 @@ c = input("Где вы живете?" )
 print("Ваши фамилия, имя", a)
 print("Ваш возраст", b)
 print("Вы живете в", c)
+
+#task 2
+x = int(input())
+t = int(input())
+z = round((9*math.pi*t+10*math.cos(x))/(math.sqrt(t)-math.fabs(math.sin(t)))*math.pow(math.e, x), 2)
+print(z)
