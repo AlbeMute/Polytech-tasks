58a91359759ba69d7941c8aa70e2ec37762cb3e8
AlbeMute <lopin7653@gmail.com>
2023-10-13 17:16:58 +0300

    task_3

--- Maksimov_SS_03_3.py
+++ Maksimov_SS_03_3.py
@@ -1,3 +1,8 @@
+'''
+The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears
+only once in the original string,or ")" if that character appears more than once in the original string. 
+Ignore capitalization when determining if a character is a duplicate.
+'''
 def duplicate(word):
     word = word.lower()
     return "".join([")" if word.count(char) > 1 else "(" for char in word])
