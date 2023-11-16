048b9325c95252f640bdbb111ba5c1b4120ef7b4
AlbeMute <lopin7653@gmail.com>
2023-11-16 23:08:15 +0300

    task 7*

--- Maksimov_SS_03_7.py
+++ Maksimov_SS_03_7.py
@@ -1,24 +1,22 @@
-def count_bouncy_numbers(N):
-    # Initialize dp array
-    dp = [[0] * 10 for _ in range(N+1)]
+''' 	
+A bouncy number is a positive integer whose digits neither increase nor decrease. 
+For example, 1235 is an increasing number, 5321 is a decreasing number, and 2351 is a bouncy number. 
+By definition, all numbers under 100 are non-bouncy, and 101 is the first bouncy number.
 
-    # Initialize dp array for N=1
-    for d in range(10):
-        dp[1][d] = 1
+Determining if a number is bouncy is easy, but counting all bouncy numbers with N digits can be challenging for large values of N. 
+To complete this kata, you must write a function that takes a number N and return the count of bouncy numbers with N digits. 
+For example, a "4 digit" number includes zero-padded, smaller numbers, such as 0001, 0002, up to 9999.
 
-    # Calculate dp array for N > 1
-    for n in range(2, N+1):
-        for d in range(10):
-            for x in range(d, 10):
-                dp[n][d] += dp[n-1][x]
+For clarification, the bouncy numbers between 100 and 125 are: 101, 102, 103, 104, 105, 106, 107, 108, 109, 120, and 121.
+'''
 
-    # Calculate total count
-    count = 0
-    for d in range(10):
-        count += dp[N][d]
 
-    return count
 
-# Тестовый пример
-print(count_bouncy_numbers(4)) # Output: 90
+
+
+
+
+
+
+
 
